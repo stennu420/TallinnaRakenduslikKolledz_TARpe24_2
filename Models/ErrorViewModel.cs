d93434b74839fa6e585bd494e7df2988240f4137
stennu420 <stenver.lindmaa@gmail.com>
2026-02-13 20:27:49 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TallinnaRakenduslikKolled__TARpe24.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
