7ad6778c9963cae99c973aea6c46f2767fad21e4
stennu420 <stenver.lindmaa@gmail.com>
2026-02-17 22:02:44 +0200

    Add Department Controller and Views

--- Data/CollegeContext.cs
+++ Data/CollegeContext.cs
@@ -15,5 +15,6 @@ namespace TallinnaRakenduslikKolled__TARpe24.Data
         }
 
         public DbSet<TallinnaRakenduslikKolled__TARpe24.Models.Course> Course { get; set; } = default!;
+        public DbSet<TallinnaRakenduslikKolled__TARpe24.Models.Department> Department { get; set; } = default!;
     }
 }
